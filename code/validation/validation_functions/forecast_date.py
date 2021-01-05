d936626ade5dd81e6ef3f706bf31620d2cd6da3b
John Levander <johnlevander@Johns-iMac.fios-router.home>
2021-01-04 23:31:13 -0500

    updating to scenario hub spec

--- code/validation/validation_functions/forecast_date.py
+++ code/validation/validation_functions/forecast_date.py
@@ -5,14 +5,14 @@ import os
 def filename_match_forecast_date(filepath):
     df = pd.read_csv(filepath)
     file_forecast_date = os.path.basename(os.path.basename(filepath))[:10]
-    forecast_date_column = set(list(df['forecast_date']))
+    forecast_date_column = set(list(df['model_projection_date']))
     if len(forecast_date_column) > 1:
-        return True, ["FORECAST DATE ERROR: %s has multiple forecast dates: %s. Forecast date must be unique" % (
+        return True, ["PROJECTION DATE ERROR: %s has multiple model projection dates: %s. Projection date must be unique" % (
             filepath, forecast_date_column)]
     else:
         forecast_date_column = forecast_date_column.pop()
         if (file_forecast_date != forecast_date_column):
-            return True, ["FORECAST DATE ERROR %s forecast filename date %s does match forecast_date column %s" % (
+            return True, ["PROJECTION DATE ERROR %s forecast filename date %s does match forecast_date column %s" % (
                 filepath, file_forecast_date, forecast_date_column)]
         else:
             return False, "no errors"
